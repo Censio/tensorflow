ea13119169440063b7199189eb0c025060a8bd81
Antoine Dubois-Rande <antoine@cens.io>
2016-08-25 14:49:35 -0400

    Remove crash code and copy sources via the binding script.

--- tensorflow/pablo/android/generate_bindings.sh
+++ tensorflow/pablo/android/generate_bindings.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 JAVA_WRAPPER_PATH=src/io/cens/android/onboard/crash
+CRASH_REPO=/Users/antoine/eng/mobile-crash-sdk
 
 rm -r $JAVA_WRAPPER_PATH
 mkdir -p $JAVA_WRAPPER_PATH
+find jni -name "*.hpp" -type f -delete
+find jni -name "*.h" -type f -delete
+find jni -name "*.cpp" -type f -delete
+find jni -name "*.cxx" -type f -delete
+find jni -name "*.cc" -type f -delete
+find jni -name "*.c" -type f -delete
+
+cp -R $CRASH_REPO/src/ jni
 swig -c++ -java -package io.cens.android.onboard.crash -I../src -outdir $JAVA_WRAPPER_PATH jni/CrashDetector.i
